4ed1097eb6dc3e775fa23d3ea95eaf5978446d22
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2023-12-04 11:14:16 +0530

    mongodb remote credentials added

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const uri = `mongodb+srv://shafayetahmad1:1wiZf7sw9VXdSg4b@cluster0.dawr9mq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dawr9mq.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri = `mongodb://localhost:27017`;
+// const uri = `mongodb://localhost:27017`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
